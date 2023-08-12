3694615e443735134c1bec06539f4b3cf51aa5f5
Lucie Genevey <l.genevey@gmail.com>
2023-08-12 21:16:30 +0200

    sdff

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -5,7 +5,8 @@ service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DATABASE;"
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
+#mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
+mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('1234');"
 mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p 1234 shutdown
