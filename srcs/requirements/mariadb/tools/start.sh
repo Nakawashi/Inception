fbf89aa18c0ced86292de33e133546d776421457
Lucie Genevey <l.genevey@gmail.com>
2023-08-12 13:05:16 +0200

    Update start.sh

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,28 +1,32 @@
 #!/bin/sh
 
-# Load environment variables from .env
-if [ -f /docker-entrypoint-initdb.d/.env ]; then
-    . /docker-entrypoint-initdb.d/.env
-fi
+# Cheking if the database is already created
+cat .setup 2>/dev/null
 
-# Run the server in the background and wait for it to start up before creating the database
-mariadb-install-db --datadir=/var/lib/mysql \
+if [ $? -ne 0 ]; then
+
+  # Run the server in the background and wait for it to start up before creating the database
+  mariadb-install-db --datadir=/var/lib/mysql \
     --auth-root-authentication-method=normal
 
-# Give permissions to the database for the user mysql
-chown -R mysql:mysql /var/lib/mysql
-chown -R mysql:mysql /run/mysqld
+  # Give permissions to the database for the user mysql
+  chown -R mysql:mysql /var/lib/mysql
+  chown -R mysql:mysql /run/mysqld
 
-# Start the server
-mysqld_safe --datadir=/var/lib/mysql &
+  # Start the server
+  mysqld_safe --datadir=/var/lib/mysql &
 
-# Wait until mariadb is ready to accept connections
-while ! mysqladmin ping -h "localhost" --silent; do
+  # Wait until mariadb is ready to accept connections
+  while ! mysqladmin ping -h "mariadb" --silent; do
     sleep 1
-done
+  done
 
-# Create database from .sql file
-eval "echo \"$(cat /docker-entrypoint-initdb.d/init_db.sql)\"" | mariadb
+  # Create database from .sql file
+  eval "echo \"$(cat /docker-entrypoint-initdb.d/init_db.sql)\"" | mariadb
+  touch .setup
+else
+  echo "Database already created and ready"
+fi
 
 # Run the server
-exec mysqld_safe --datadir=/var/lib/mysql
+mysqld_safe --datadir=/var/lib/mysql
