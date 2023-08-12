b7f2b442a31a6fec60be9eb472b0ce225c7b9654
Lucie Genevey <l.genevey@gmail.com>
2023-08-12 13:12:13 +0200

    Update start.sh

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,32 +1,28 @@
 #!/bin/sh
 
-# Cheking if the database is already created
-cat .setup 2>/dev/null
-
-if [ $? -ne 0 ]; then
-
-  # Run the server in the background and wait for it to start up before creating the database
-  mariadb-install-db --datadir=/var/lib/mysql \
-    --auth-root-authentication-method=normal
-
-  # Give permissions to the database for the user mysql
-  chown -R mysql:mysql /var/lib/mysql
-  chown -R mysql:mysql /run/mysqld
-
-  # Start the server
-  mysqld_safe --datadir=/var/lib/mysql &
-
-  # Wait until mariadb is ready to accept connections
-  while ! mysqladmin ping -h "mariadb" --silent; do
-    sleep 1
-  done
-
-  # Create database from .sql file
-  eval "echo \"$(cat /docker-entrypoint-initdb.d/init_db.sql)\"" | mariadb
-  touch .setup
-else
-  echo "Database already created and ready"
+# Load environment variables from .env
+if [ -f /docker-entrypoint-initdb.d/.env ]; then
+    . /docker-entrypoint-initdb.d/.env
 fi
 
+# Run the server in the background and wait for it to start up before creating the database
+mariadb-install-db --datadir=/var/lib/mysql \
+    --auth-root-authentication-method=normal
+
+# Give permissions to the database for the user mysql
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /run/mysqld
+
+# Start the server
+mysqld_safe --datadir=/var/lib/mysql &
+
+# Wait until mariadb is ready to accept connections
+while ! mysqladmin ping -h "localhost" --silent; do
+    sleep 1
+done
+
+# Create database from .sql file
+eval "echo \"$(cat /docker-entrypoint-initdb.d/init_db.sql)\"" | mariadb
+
 # Run the server
-mysqld_safe --datadir=/var/lib/mysql
+exec mysqld_safe --datadir=/var/lib/mysql
