a8201d26f7039846b7fb872af066a1f167829fea
Lucie Genevey <l.genevey@gmail.com>
2023-08-13 04:42:41 +0200

    erreur docker run mariadb

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -2,11 +2,15 @@
 
 service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DATABASE;"
+WP_DATABASE="wordpress_db"
+MYSQL_USER="user"
+MYSQL_PASSWORD="1234"
+MYSQL_ROOT_PASS="1234"
+
+mysql -e "CREATE DATABASE IF NOT EXISTS '$WP_DATABASE';"
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-#mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
-mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('1234');"
+mysql -e "GRANT ALL PRIVILEGES ON '$WP_DATABASE'.* TO '$MYSQL_USER'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASS';"
 mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p 1234 shutdown
