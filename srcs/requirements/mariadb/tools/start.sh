a6adc93594c2e26eb294266564bccc4325b2be55
Lucie <l.genevey@gmail.com>
2023-08-21 00:16:21 +0200

    root denied

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-service mysql start
+service mysql start;
 
-WP_DATABASE="wordpress_db"
-MYSQL_USER="user"
+MYSQL_DATABASE="wp_inception"
+MYSQL_ROOT_PASSWORD="coucou"
+MYSQL_USER="naka"
 MYSQL_PASSWORD="1234"
-MYSQL_ROOT_PASS="1234"
 
-mysql -e "CREATE DATABASE IF NOT EXISTS '$WP_DATABASE';"
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON '$WP_DATABASE'.* TO '$MYSQL_USER'@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASS';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p 1234 shutdown
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 
-exec "$@"
+exec mysqld_safe
