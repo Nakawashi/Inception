3ad12c97dbb6c270fc834c4c39be70599a5b0d47
Lucie Genevey <l.genevey@gmail.com>
2023-08-24 18:15:26 +0200

    Update start.sh

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-service mysql start;
+service mysql start
+sleep 0.5
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown;
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 
-mysqld;
+mysqld
