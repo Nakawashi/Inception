b14f23a53c9fa26db1e094352b596f2b0fe03e75
Lucie Genevey <l.genevey@gmail.com>
2023-08-27 20:35:47 +0200

    resolve: php.ini -> php.conf for php-fpm

--- srcs/requirements/wordpress/tools/wp_install.sh
+++ srcs/requirements/wordpress/tools/wp_install.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-# Attendre que le serveur MySQL soit prêt
-# while ! mysql -h mariadb -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE &>/dev/null; do
-#   sleep 3
-# done
-
-# Installer WP-CLI
+# Install WP-CLI
 apt-get update -y
 apt-get install curl -y
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-# Aller dans le répertoire WordPress
-cd /var/www/wordpress
+# Move where Wordpress is gonna be installed, dl WP, remove tar.gz archive
+cd /var/www/
+wget http://fr.wordpress.org/latest-fr_FR.tar.gz
+tar -xzvf latest-fr_FR.tar.gz
+rm latest-fr_FR.tar.gz
+
 
 # Télécharger WordPress si le fichier de configuration n'existe pas
 if [ ! -f /var/www/wordpress/wp-config.php]; then
-	wp core download --allow-root
-
-	echo "coucou c moi"
+	# wp core download --allow-root
 	
 	# Créer le fichier de configuration WordPress
 	wp config create \
@@ -46,6 +43,7 @@ if [ ! -f /var/www/wordpress/wp-config.php]; then
 		--user_pass=$USER1_PASSWORD \
 		--path='/var/www/wordpress' >> /log.txt
 fi
+
 
 # Exécuter la commande passée en argument (peut être utilisée pour démarrer php-fpm ou autre)
 exec "$@"
